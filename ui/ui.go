45f2c4e741686aee882a2a9936902aca558df71e
jacob_shih <jacob_shih@hotmail.com>
2019-12-21 03:11:00 +0800

    rename ComingSoonContents to WarningContents, add SomethingWrongMessage.

--- ui/ui.go
+++ ui/ui.go
@@ -23,9 +23,11 @@ const (
 	URIComingSoon500x500        = "https://imgur.com/nGHRg4P.png"
 	URIComingSoon1920x823       = "https://imgur.com/qwPOOR2.png"
 	URIUnderConstruction256x256 = "https://imgur.com/rBII01U.png"
+	URISomethingWrong800x329    = "https://imgur.com/qG9iIo8.png"
 
 	TextComingSoon        = "Coming soon"
 	TextUnderConstruction = "Under construction"
+	TextSomethingWrong    = "Something went wrong"
 
 	ColorAcidGreen = "#b0bf1a"
 	ColorAero      = "#7cb9e8"
@@ -81,16 +83,74 @@ func ComingSoonContents(text string) *ExtCarouselContainer {
 	return &contents
 }
 
+// WarningContents function generates ExtCarouselContainer for warning box.
+func WarningContents(text, img string, ratio linebot.FlexImageAspectRatioType) *ExtCarouselContainer {
+	contents := ExtCarouselContainer{
+		CarouselContainer: linebot.CarouselContainer{
+			Type: linebot.FlexContainerTypeCarousel,
+		},
+		Contents: []*ExtBubbleContainer{
+			{
+				Type:      linebot.FlexContainerTypeBubble,
+				Direction: linebot.FlexBubbleDirectionTypeLTR,
+				Header: &ExtBoxComponent{
+					BoxComponent: linebot.BoxComponent{
+						Type:   linebot.FlexComponentTypeBox,
+						Layout: linebot.FlexBoxLayoutTypeVertical,
+						Contents: []linebot.FlexComponent{
+							&linebot.TextComponent{
+								Type:   linebot.FlexComponentTypeText,
+								Text:   text,
+								Align:  linebot.FlexComponentAlignTypeCenter,
+								Margin: linebot.FlexComponentMarginTypeNone,
+								Size:   linebot.FlexTextSizeTypeLg,
+								Color:  ColorAero,
+							},
+						},
+					},
+				},
+				Hero: &linebot.ImageComponent{
+					Type:        linebot.FlexComponentTypeImage,
+					AspectRatio: ratio,
+					AspectMode:  linebot.FlexImageAspectModeTypeCover,
+					URL:         img,
+					Size:        linebot.FlexImageSizeTypeFull,
+				},
+			},
+		},
+	}
+	return &contents
+}
+
+// SomethingWrongContents function generates ExtCarouselContainer for warning box.
+func SomethingWrongContents() *ExtCarouselContainer {
+	uri := URISomethingWrong800x329
+	ratio := linebot.FlexImageAspectRatioType16to9
+	return WarningContents(TextSomethingWrong, uri, ratio)
+}
+
+// SomethingWrongMessage function generates FlexMessage for something wrong message.
+func SomethingWrongMessage() *linebot.FlexMessage {
+	uri := URISomethingWrong800x329
+	ratio := linebot.FlexImageAspectRatioType16to9
+	contents := WarningContents(TextSomethingWrong, uri, ratio)
+	return linebot.NewFlexMessage(TextSomethingWrong, contents)
+}
+
 // ComingSoonMessage function generates FlexMessage for coming soon message.
 func ComingSoonMessage() *linebot.FlexMessage {
-	contents := ComingSoonContents(TextComingSoon)
+	uri := URIComingSoon500x500
+	ratio := linebot.FlexImageAspectRatioType1to1
+	contents := WarningContents(TextComingSoon, uri, ratio)
 	return linebot.NewFlexMessage(TextComingSoon, contents)
 }
 
 // UnderConstructionMessage function generates FlexMessage for under
 // construction message.
 func UnderConstructionMessage() *linebot.FlexMessage {
-	contents := ComingSoonContents(TextUnderConstruction)
+	uri := URIUnderConstruction256x256
+	ratio := linebot.FlexImageAspectRatioType1to1
+	contents := WarningContents(TextUnderConstruction, uri, ratio)
 	return linebot.NewFlexMessage(TextUnderConstruction, contents)
 }
 
